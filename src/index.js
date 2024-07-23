fdf88b7ed26be8babf033b5641944739d279de87
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2024-07-23 23:22:48 +0200

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let ans = '';
+    const str = Math.abs(n) + '';
+    for (let i = str.length-1; i>=0; i--) {
+        ans+=str[i];
+    }
+    return +ans;
 }
